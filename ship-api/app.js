9fcd66ebae6152c45d6bee3d1e0684f6bf21c1e1
dengzhou <dengzhou@sinovoice.com.cn>
2017-11-30 19:39:35 +0800

    log4jsconfig

--- ship-api/app.js
+++ ship-api/app.js
@@ -16,8 +16,7 @@ let logconfig = require('./logs/logconfig');
 
 log4js.configure(logconfig.config);
 let log4 = log4js.getLogger('normal');
-log4.setLevel('INFO');
-app.use(log4js.connectLogger(logger, { level: log4js.levels.INFO }));
+
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
--- ship-api/logs/logconfig.js
+++ ship-api/logs/logconfig.js
@@ -1,15 +1,33 @@
 module.exports = {
     config: {
-        appenders: [
-            { type: 'console' }, //控制台输出
-            {
-                type: 'file', //文件输出
-                filename: 'logs/access.log',//输出日志的文件夹/文件名，不会自动生成文件夹，请先自行创建logs文件夹
-                maxLogSize: 1024 * 12,//一个文件的大小，超出后会自动新生成一个文件
-                backups: 3,//备份的文件数量
-                category: 'normal'
+        replaceConsole: true,
+        appenders: {
+            stdout: {//控制台输出
+                type: 'stdout'
+            },
+            req: {//请求日志
+                type: 'dateFile',
+                filename: 'logs/reqlog/',
+                pattern: 'req-yyyy-MM-dd.log',
+                alwaysIncludePattern: true
+            },
+            err: {//错误日志
+                type: 'dateFile',
+                filename: 'logs/errlog/',
+                pattern: 'err-yyyy-MM-dd.log',
+                alwaysIncludePattern: true
+            },
+            oth: {//其他日志
+                type: 'dateFile',
+                filename: 'logs/othlog/',
+                pattern: 'oth-yyyy-MM-dd.log',
+                alwaysIncludePattern: true
             }
-        ],
-        replaceConsole: true
+        },
+        categories: {
+            default: { appenders: ['stdout', 'req'], level: 'debug' },//appenders:采用的appender,取appenders项,level:设置级别
+            err: { appenders: ['stdout', 'err'], level: 'error' },
+            oth: { appenders: ['stdout', 'oth'], level: 'info' }
+        }
     }
 }
